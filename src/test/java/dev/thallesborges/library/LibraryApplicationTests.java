bf0db934ed2e6acfe11f20b526d4a24c9bc14601
Thalles Borges <dev.thallesborges@gmail.com>
2026-05-04 20:42:42 -0300

    feat(test): adiciona Testcontainers e configura ambiente de testes

--- src/test/java/dev/thallesborges/library/LibraryApplicationTests.java
+++ src/test/java/dev/thallesborges/library/LibraryApplicationTests.java
@@ -2,9 +2,20 @@ package dev.thallesborges.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class LibraryApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer postgres = new  PostgreSQLContainer("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
